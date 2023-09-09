fabc32d51b1c6e767d506c8254ed648eeebe8f82
SonSeongeun <wizmans1414@gmail.com>
2023-09-09 18:40:48 +0900

    '1. 모든 단어 보기' 기능 추가

--- src/main/java/com/mycom/word/Word.java
+++ src/main/java/com/mycom/word/Word.java
@@ -5,6 +5,20 @@ public class Word {
     private int id;
     private int level;
     private String word;
+
+    @Override
+    public String toString() {
+
+        String slevel = "";
+        for(int i = 0; i < level ; i++) slevel += "*";
+        String str = String.format("%-3s", slevel)
+                    + String.format("%15s", word)
+                    + "  "
+                    + meaning;
+        return str;
+
+    }
+
     private String meaning;
 
     Word(){}
